bbbe9df645dc0a42c960782813604b9e69a3f52c
Marken <nctuan.ityu@gmail.com>
2021-09-19 11:46:00 +0100

    practical

---
+++ bloc_flutter/lib/bloc_basis/counter_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:bloc_flutter/bloc_basis/counter_event.dart';
+
+class CounterBloc {
+  int _counter = 0;
+
+  final _counterStateController = StreamController<int>();
+  StreamSink<int> get _inCounter => _counterStateController.sink;
+  // For State, exposing only a stream which outputs data
+  Stream<int> get counter => _counterStateController.stream;
+
+  // For Events, exposing only a sink which is an input
+  final _counterEventController = StreamController<CounterEvent>();
+  Sink<CounterEvent> get counterEventSink => _counterEventController.sink;
+
+  CounterBloc() {
+    _counterEventController.stream.listen(_mapEventToState);
+  }
+
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncrementEvent) {
+      _counter++;
+    } else {
+      _counter--;
+    }
+
+    _inCounter.add(_counter);
+  }
+
+  void dispose() {
+    _counterEventController.close();
+    _counterStateController.close();
+  }
+}
