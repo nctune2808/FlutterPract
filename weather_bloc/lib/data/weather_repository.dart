ce3743501dfd91aa5c02d9ea1106bfd4ab06b604
Marken <nctuan.ityu@gmail.com>
2021-09-19 14:48:37 +0100

    init

---
+++ weather_bloc/lib/data/weather_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:weather_bloc/model/weather.dart';
+
+abstract class WeatherRepository {
+  /// Throws [NetworkException].
+  Future<Weather> fetchWeather(String cityName);
+}
+
+class FakeWeatherRepository implements WeatherRepository {
+  @override
+  Future<Weather> fetchWeather(String cityName) {
+    // Simulate network delay
+    return Future.delayed(
+      Duration(seconds: 1),
+      () {
+        final random = Random();
+
+        // // Simulate some network exception
+        // if (random.nextBool()) {
+        //   throw NetworkException();
+        // }
+
+        // Return "fetched" weather
+        return Weather(
+          cityName: cityName,
+          // Temperature between 20 and 35.99
+          temperatureCelsius: 20 + random.nextInt(15) + random.nextDouble(),
+        );
+      },
+    );
+  }
+}
