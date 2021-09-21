1711dd2c5c2e2db40915b4f4b94a9b4755b3370c
Marken <nctuan.ityu@gmail.com>
2021-09-21 10:22:28 +0100

    2

---
+++ flashcard_flutter/lib/flashcard.dart
@@ -0,0 +1,6 @@
+class Flashcard {
+  final String front;
+  final String back;
+
+  Flashcard({required this.front, required this.back});
+}
---
+++ flashcard_flutter/lib/flashcardview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FlashcardView extends StatelessWidget {
+  final String text;
+
+  // Constructor
+  const FlashcardView({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
