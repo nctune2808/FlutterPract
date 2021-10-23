c8734061fb2f8e793bd24fd6229a373711f2d62f
Marken <nctuan.ityu@gmail.com>
2021-10-23 18:58:59 +0100

    pause

--- tolo/lib/src/profile/profile_bloc.dart
+++ tolo/lib/src/profile/profile_bloc.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tolo/src/profile/profile_repository.dart';
 import 'package:tolo/utility/state/status.dart';
 
 part 'profile_event.dart';
@@ -8,6 +11,7 @@ part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
   ProfileBloc() : super(ProfileInitState());
+  ProfileRepository _profRepo = ProfileRepository();
   final _imgPicker = ImagePicker();
 
   @override
@@ -18,6 +22,9 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
       yield state.copyWith(isImagePickerVisible: false);
       final pickedImg = await _imgPicker.getImage(source: event.imageSource);
       if (pickedImg == null) return;
+      // _profRepo.uploadImage(File(pickedImg.path));
+
+      // yield state.copyWith()
       yield state.copyWith(avatarPath: pickedImg.path);
     } else if (event is ImgPathProfileEvent) {
       yield state.copyWith(avatarPath: event.avatarPath);
---
+++ tolo/lib/src/profile/profile_repository.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProfileRepository {
+  // Future<void> saveImages(File _images, DocumentReference ref) async {
+  //   String imageURL = await uploadImage(_images);
+  //   ref.update({
+  //     "images": FieldValue.arrayUnion([imageURL])
+  //   });
+  // }
+
+  // Future uploadImage(File file) async {
+
+  //   StorageReference storageReference;
+
+  //   try {
+  //     final fileName = DateTime.now().toIso8601String();
+  //     final storageReference =
+  //         FirebaseStorage.instance.ref().child("images/$fileName");
+
+  //     final StorageUploadTask uploadTask = storageReference.putFile(file);
+  //     final StorageTaskSnapshot downloadUrl = (await uploadTask.onComplete);
+  //     final String url = (await downloadUrl.ref.getDownloadURL());
+  //     print("URL is $url");
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
+}
