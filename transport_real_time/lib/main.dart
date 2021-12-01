7399f68b398b7deb7664a0e62fc484d613c90d0c
Marken <nctuan.ityu@gmail.com>
2021-12-01 15:08:35 +0000

    impl google maps services for ios android

--- transport_real_time/lib/main.dart
+++ transport_real_time/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transport_real_time/views/map/map_view.dart';
 import 'package:transport_real_time/views/search/search_view.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: BusView(),
-      home: SearchView(),
+      home: MapView(),
     );
   }
 }
