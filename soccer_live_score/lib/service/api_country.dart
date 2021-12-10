01b1b92b2b142fd9cdaf9c8d859d2b5fc5b809e0
Marken <nctuan.ityu@gmail.com>
2021-12-10 20:37:00 +0000

    fetch countries when scrolling

--- soccer_live_score/lib/service/api_country.dart
+++ soccer_live_score/lib/service/api_country.dart
@@ -6,6 +6,15 @@ import 'package:soccer_live_score/model/country.dart';
 class CountryApi {
   static const apiUrl = 'https://restcountries.com/v3.1/';
 
+  static Future<List<Country>> getAllCountries() async {
+    final String cUrl = apiUrl + 'all';
+    var cRes = await http.get(Uri.parse(cUrl));
+    var cJson = jsonDecode(cRes.body) as List<dynamic>;
+    List<Country> countries =
+        cJson.map((json) => Country.fromJson(json)).toList();
+    return (countries);
+  }
+
   static Future<Country> getCountry(String name) async {
     final String cUrl = apiUrl + 'name/$name';
     var cRes = await http.get(Uri.parse(cUrl));
--- soccer_live_score/lib/src/welcome_view.dart
+++ soccer_live_score/lib/src/welcome_view.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:soccer_live_score/model/country.dart';
 import 'package:soccer_live_score/model/team.dart';
+import 'package:soccer_live_score/service/api_country.dart';
 
-class WelcomeView extends StatelessWidget {
+class WelcomeView extends StatefulWidget {
   const WelcomeView({Key? key}) : super(key: key);
+
+  @override
+  _WelcomeViewState createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  List<Country> countries = [];
+  int index = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getCountryList();
+    index;
+  }
+
+  void _getCountryList() async {
+    List<Country> countryList = await CountryApi.getAllCountries();
+    setState(() {
+      countries = countryList;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +38,13 @@ class WelcomeView extends StatelessWidget {
 
     FixedExtentScrollController fixedExtentScrollController =
         new FixedExtentScrollController();
+
     return Scaffold(
         appBar: AppBar(title: Text("Football Addicts")),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            countries.isNotEmpty ? _countryBuilder() : Container(),
             _leagueBuilder(),
             _clubBuilder(),
           ],
@@ -89,6 +116,43 @@ class WelcomeView extends StatelessWidget {
             ],
             onSelectedItemChanged: (index) => {print(index)},
           ),
+        ),
+      ],
+    );
+  }
+
+  Widget _countryBuilder() {
+    return Column(
+      children: [
+        Text("Select Country", style: TextStyle(fontSize: 20)),
+        Container(
+          height: 100,
+          color: Colors.grey,
+          child: ListWheelScrollView(
+            // controller: fixedExtentScrollController,
+            physics: FixedExtentScrollPhysics(),
+            squeeze: 2,
+            itemExtent: 80,
+            perspective: 0.01,
+            diameterRatio: 1.5,
+            // magnification: 1,
+            // useMagnifier: true,
+            children: [
+              ...countries.map((country) {
+                return Center(child: Text(country.name));
+              })
+            ],
+            onSelectedItemChanged: (selected) => {
+              setState(() {
+                index = selected;
+              })
+            },
+          ),
+        ),
+        Image.network(
+          countries[index].flag,
+          width: 100,
+          height: 100,
         ),
       ],
     );
