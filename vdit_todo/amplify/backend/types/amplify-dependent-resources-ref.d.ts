38b39b2b2700782335b36ed1808f70170d6e88c6
Tuan VDIT <tuan.nguyen@vdit.co.uk>
2022-09-30 18:16:58 +0100

    update

---
+++ vdit_todo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "VDITsupport": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
