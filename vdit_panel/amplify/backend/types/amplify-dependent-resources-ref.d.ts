995be587972b37b53d3cf4f8c0acf04f204f6670
Marken <nctuan.ityu@gmail.com>
2022-10-13 00:56:50 +0100

    gen vue ts

---
+++ vdit_panel/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "vditpanel": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
