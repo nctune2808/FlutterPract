092eae7e60ab1a67b61c85f6b9a9f3802bd8205f
Tuan VDIT <tuan.nguyen@vdit.co.uk>
2022-12-07 17:31:16 +0000

    update

--- booking_appointment/lib/clone/multiplication_table.dart
+++ booking_appointment/lib/clone/multiplication_table.dart
@@ -1,6 +1,8 @@
 import 'package:booking_appointment/data/constants.dart';
 import 'package:flutter/material.dart';
 
+import '../table_cell.dart';
+
 class MultiplicationTable extends StatefulWidget {
   @override
   _MultiplicationTableState createState() => _MultiplicationTableState();
@@ -9,100 +11,104 @@ class MultiplicationTable extends StatefulWidget {
 class _MultiplicationTableState extends State<MultiplicationTable> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: Stack(children: [
-          Wrap(
-            direction: Axis.vertical,
-            children: List.generate(32, (index) {
-              return Container(
-                height: 100,
-                width: 100,
-                child: new Column(
-                  children: [
-                    new Expanded(
-                      child: Card(
-                          child: ListTile(
-                              title: Text(index.toString()))), //your card wight
-                    ),
-                  ],
-                ),
-              );
-            }),
-          ),
-          Wrap(
-            direction: Axis.vertical,
-            children: List.generate(32, (index) {
-              int varData = index;
-              return DragTarget(
-                  onWillAccept: (data) {
-                    return true;
-                  },
-                  onAccept: (data) {},
-                  onLeave: (data) {
-                    setState(() {
-                      varData = int.parse(data.toString());
-                    });
-                    print(varData);
-                  },
-                  builder: (context, candidateData, rejectedData) => Container(
-                        child: Draggable(
-                          // onDragCompleted: () {
-                          //   setState(() {
-                          //     varData = index;
-                          //   });
-                          // },
-                          data: index,
-                          feedback: Container(
-                            height: 100,
-                            width: 100,
-                            child: new Column(
-                              children: [
-                                new Expanded(
-                                  child: Card(
-                                      color: Colors.amber.shade100,
-                                      child: ListTile(
-                                          title: Text(varData
-                                              .toString()))), //your card wight
-                                ),
-                              ],
-                            ),
-                          ),
-                          childWhenDragging: Container(
-                              height: 100,
+    return SingleChildScrollView(
+      child: Container(
+        width: datas.length.toDouble()*100,
+        height: datas.length.toDouble()*100,
+          color: Colors.white,
+          child: Stack(children: [
+            Wrap(
+              direction: Axis.vertical,
+              children: List.generate(18, (index) {
+                return Container(
+                  height: (datas[index].d * (cellHeight)),
+                  width: 100,
+                  child: new Column(
+                    children: [
+                      new Expanded(
+                        child: Card(
+                            child: ListTile(
+                                title: Text(index.toString()))), //your card wight
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ),
+            Wrap(
+              direction: Axis.vertical,
+              children: List.generate(18, (index) {
+                int varData = index;
+                return DragTarget(
+                    onWillAccept: (data) {
+                      return true;
+                    },
+                    onAccept: (data) {},
+                    onLeave: (data) {
+                      setState(() {
+                        varData = int.parse(data.toString());
+                      });
+                      print(varData);
+                    },
+                    builder: (context, candidateData, rejectedData) => Container(
+                          child: Draggable(
+                            // onDragCompleted: () {
+                            //   setState(() {
+                            //     varData = index;
+                            //   });
+                            // },
+                            data: index,
+                            feedback: Container(
+                              height: (datas[index].d * (cellHeight)),
                               width: 100,
-                              child: new Column(children: [
-                                new Expanded(
-                                  child: Card(
-                                      // color: Colors.amber,
-                                      child: ListTile(
-                                          title: Text(varData
-                                              .toString()))), //your card wight
-                                ),
-                              ])),
-                          child: Container(
-                            height: 100,
-                            width: 100,
-                            child: new Column(
-                              children: [
-                                new Expanded(
-                                  child: Card(
-                                      color: Colors.amber,
-                                      child: ListTile(
-                                          onTap: (() {
-                                            print(varData);
-                                          }),
-                                          title: Text(varData
-                                              .toString()))), //your card wight
-                                ),
-                              ],
+                              child: new Column(
+                                children: [
+                                  new Expanded(
+                                    child: Card(
+                                        color: Colors.amber.shade100,
+                                        child: ListTile(
+                                            title: Text(varData
+                                                .toString()))), //your card wight
+                                  ),
+                                ],
+                              ),
+                            ),
+                            childWhenDragging: Container(
+                                height: (datas[index].d * (cellHeight)),
+                                width: 100,
+                                child: new Column(children: [
+                                  new Expanded(
+                                    child: Card(
+                                        // color: Colors.amber,
+                                        child: ListTile(
+                                            title: Text(varData
+                                                .toString()))), //your card wight
+                                  ),
+                                ])),
+                            child: Container(
+                              height: (datas[index].d * (cellHeight)),
+                              width: 100,
+                              child: new Column(
+                                children: [
+                                  new Expanded(
+                                    child: Card(
+                                        color: Colors.amber,
+                                        child: ListTile(
+                                            onTap: (() {
+                                              print(varData);
+                                            }),
+                                            title: Text(varData
+                                                .toString()))), //your card wight
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ));
-            }),
-          ),
-        ]));
+                        ));
+              }),
+            ),
+          ])),
+    );
   }
 }
 
